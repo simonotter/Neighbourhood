27e83028860f1261d0277ad6fe7172327f80e4b9
Simon Otter <simonotter@gmail.com>
2016-08-29 08:27:04 +0100

    Improve comments in ViewModel

--- js/app.js
+++ js/app.js
@@ -5,18 +5,23 @@ var map;
 var ViewModel = function() {
     var self = this;
 
+    // Create the places array
     this.placeList = ko.observableArray([]);
-
     places.forEach(function(place) {
         self.placeList.push(new Place(place));
     });
 
-    this.currentPlace = ko.observable(null);
-
+    // Creat the list of place types
+    // TODO: Move this data to the model
     this.placeTypes = ko.observableArray(['Show all', 'Cafe', 'Groceries', 'Restaurant']);
 
+    //Set the initial place to nothing
+    this.currentPlace = ko.observable(null);
+
+    //Set the initial type to the first type (Show all)
     this.chosenType = ko.observable(this.placeTypes()[0]);
 
+    //Filter the places based on the chosen type
     this.filteredPlaces = ko.computed(function() {
         var filter = self.chosenType();
 
@@ -29,7 +34,7 @@ var ViewModel = function() {
         }
     });
 
-    // Behaviours
+    // Set the current place
     this.setCurrentPlace = function(clickedPlace) {
         self.currentPlace(clickedPlace);
 
@@ -40,8 +45,17 @@ var ViewModel = function() {
 
         // Make map marker visible
         clickedPlace.marker.setMap(map);
+
+        clickedPlace.marker.addListener('click', function() {
+            if (clickedPlace.marker.getAnimation() !== null) {
+              clickedPlace.marker.setAnimation(null);
+            } else {
+              clickedPlace.marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        })
     };
 
+    // Set the current type
     this.setCurrentType = function(clickedType) {
         self.chosenType(clickedType);
         self.currentPlace(null);
